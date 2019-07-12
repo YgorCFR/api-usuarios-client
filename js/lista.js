d0e3ee5b09fb9a7d65cea2bc4deddbe83f2f5c33
ygor.rolim <ygor.rolim@radixeng.com.br>
2019-07-12 11:18:22 -0300

    Client finalizado.

--- js/lista.js
+++ js/lista.js
@@ -6,7 +6,22 @@ $( document ).ready(function() {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
             console.log(myArr);
-            document.getElementById("linha_lista_email").innerHTML = myArr[0].email;
+            
+                document.getElementById("linha_lista_email").innerHTML = myArr[0].email;
+                document.getElementById("linha_lista_pontos").innerHTML = myArr[0].pontos;
+                document.getElementById("linha_lista_nome").innerHTML = myArr[0].nome;
+
+                document.getElementById("linha_lista_email_2").innerHTML = myArr[1].email;
+                document.getElementById("linha_lista_pontos_2").innerHTML = myArr[1].pontos;
+                document.getElementById("linha_lista_nome_2").innerHTML = myArr[1].nome;
+
+                document.getElementById("linha_lista_email_3").innerHTML = myArr[2].email;
+                document.getElementById("linha_lista_pontos_3").innerHTML = myArr[2].pontos;
+                document.getElementById("linha_lista_nome_3").innerHTML = myArr[2].nome;
+
+                document.getElementById("linha_lista_email_4").innerHTML = myArr[3].email;
+                document.getElementById("linha_lista_pontos_4").innerHTML = myArr[3].pontos;
+                document.getElementById("linha_lista_nome_4").innerHTML = myArr[3].nome;
         }
     };
     xmlhttp.open('GET', url, true);
